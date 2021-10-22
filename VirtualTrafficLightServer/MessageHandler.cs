cbbe35cc5f4ce712ff19b37a661993d6916d6866
GoodSSenDev <yoodonggeun7@naver.com>
2021-10-23 04:46:57 +1300

    Changed the server to Websocket & Added Basic form of message Handler

---
+++ VirtualTrafficLightServer/MessageHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VirtualTrafficLightCoreLibrary.Server;
+
+namespace VirtualTrafficLightServer
+{
+    public class MessageHandler
+    {
+
+        /// <summary>
+        ///  Bind the Channel so when they gets the message they can wo
+        /// </summary>
+        /// <param name="channel">Channel</param>
+        public void Bind(ServerChannel channel)
+            => channel.OnMessage(async (m, channelPath) => {
+
+                if(m.Distance <= 100)
+                {
+                    //add lane
+                    //return 
+                }
+
+
+                if (m.IsDistanceShrinking == false)
+                {
+                    //remove
+                }
+
+            });
+    }
+}
